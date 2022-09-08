670270c3977d061440ddc4f91f242f72151084ed
Saotomefirst <jorge.appleseed@gmail.com>
2022-09-07 23:29:54 -0300

    deleted some unneded comments

--- app/src/main/java/com/saotome/imed_app/ui/fragments/EditDrugFragment.kt
+++ app/src/main/java/com/saotome/imed_app/ui/fragments/EditDrugFragment.kt
@@ -9,18 +9,10 @@ import androidx.navigation.fragment.findNavController
 import com.saotome.imed_app.R
 import com.saotome.imed_app.databinding.FragmentEditDrugBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EditDrug.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditDrug : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -46,6 +38,7 @@ class EditDrug : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
         _binding?.editDrugCancel?.setOnClickListener {
             findNavController().navigate(R.id.action_EditDrug_Cancel)
         }
